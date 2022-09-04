48df3d752c97e6d82a8506ee4977fc5fd0287277
cloer <chltjrrb97@gmail.com>
2022-09-03 23:12:56 -0400

    FIX: a bug that snake returns it's body way when keyboard is pressed fast

--- snake.js
+++ snake.js
@@ -1,13 +1,15 @@
 import { isEqualPosition, isOutsideGrid } from './lib.js';
 
 export default class Snake {
-  #direction = { x: 0, y: 0 };
   body = [{ x: 11, y: 11 }];
   isDead = false;
+  #direction = { x: 0, y: 0 };
+  #inputDirection = { x: 0, y: 0 };
   constructor() {
     this.#addListener();
   }
   move() {
+    this.#direction = this.#inputDirection;
     const newHead = { x: this.body[0].x + this.#direction.x, y: this.body[0].y + this.#direction.y };
 
     if (isOutsideGrid(newHead) || this.body.slice(1).some(isEqualPosition(newHead))) this.isDead = true;
@@ -27,16 +29,16 @@ export default class Snake {
     window.addEventListener('keydown', (e) => {
       switch (e.key) {
         case 'ArrowUp':
-          if (this.#direction.y === 0) this.#direction = { x: 0, y: -1 };
+          if (this.#direction.y === 0) this.#inputDirection = { x: 0, y: -1 };
           break;
         case 'ArrowDown':
-          if (this.#direction.y === 0) this.#direction = { x: 0, y: 1 };
+          if (this.#direction.y === 0) this.#inputDirection = { x: 0, y: 1 };
           break;
         case 'ArrowLeft':
-          if (this.#direction.x === 0) this.#direction = { x: -1, y: 0 };
+          if (this.#direction.x === 0) this.#inputDirection = { x: -1, y: 0 };
           break;
         case 'ArrowRight':
-          if (this.#direction.x === 0) this.#direction = { x: 1, y: 0 };
+          if (this.#direction.x === 0) this.#inputDirection = { x: 1, y: 0 };
           break;
       }
     });
--- snakeGame.js
+++ snakeGame.js
@@ -42,6 +42,7 @@ class Game {
     if (secondsSinceLastRender < 1 / this.#gameSpeed) return;
     this.#lastRenderTime = currentTime;
 
+    console.log('render');
     this.#update();
     this.#draw();
   }
